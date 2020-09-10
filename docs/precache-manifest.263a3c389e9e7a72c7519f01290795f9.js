94c98c6578edccfa636f8dca2bb1bc474b8b4f61
keirlewis <keirl@sipios.com>
2020-09-10 10:00:36 +0200

    Add docs

---
+++ docs/precache-manifest.263a3c389e9e7a72c7519f01290795f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63a36d21a6fe5e49c35c37cad6eb057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b100e913b76134ece37",
+    "url": "/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "e3220d23511921306598",
+    "url": "/static/js/2.eb934d43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb934d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b100e913b76134ece37",
+    "url": "/static/js/main.622d880d.chunk.js"
+  },
+  {
+    "revision": "074dc4a9626fe9e1eea9",
+    "url": "/static/js/runtime-main.8fd6c578.js"
+  },
+  {
+    "revision": "d017bff30705473236416e5cf9e8c685",
+    "url": "/static/media/logo.d017bff3.webp"
+  }
+]);
